5427c7a3ba13cd8c2e85b141595093cb0945cde5
Benjamin Yolken <yolken@gmail.com>
2019-09-01 18:02:33 -0700

    Fix publishing script

--- scripts/publish_blog.sh
+++ scripts/publish_blog.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -ev
+set -e
 
-SCRIPT_DIR=`dirname "$0"`
-BLOG_DIR=SCRIPT_DIR/..
-SITE_DIR=BLOG_DIR/site
-PUB_DIR=`mktemp -d`
+SCRIPT_DIR=$(dirname "$0")
+BLOG_DIR=$SCRIPT_DIR/..
+SITE_DIR=$BLOG_DIR/site
+PUB_DIR=$(mktemp -d)
 
 pushd $SITE_DIR
 JEKYLL_ENV=production jekyll build -d $PUB_DIR
@@ -13,9 +13,17 @@ popd
 pushd $BLOG_DIR
 git checkout gh-pages
 cp -R $PUB_DIR/* .
-git add .
-git commit -a -m "Update blog"
-git push origin gh-pages
+CHANGED=$(git diff-index --name-only HEAD --)
+
+if [ -n "$CHANGED" ]; then
+  echo "Publishing changes"
+  git add .
+  git commit -a -m "Update blog"
+  git push origin gh-pages
+else
+  echo "No changes to publish"
+fi
+
 git checkout master
 popd
 
